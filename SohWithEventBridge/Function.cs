eb240ae91d8b390c41655b30476c58df4615debb
James Matson <James.Matson@api.net.au>
2024-04-28 13:34:11 +1000

    Fixed outstanding errors and logic problems. Function complete.

--- SohWithEventBridge/Function.cs
+++ SohWithEventBridge/Function.cs
@@ -1,5 +1,7 @@
 using Amazon.Lambda.CloudWatchEvents;
 using Amazon.Lambda.Core;
+using SohWithEventBridge.Models;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -10,7 +12,19 @@ public class Function
 { 
     public string StockOnHandEventHandler(CloudWatchEvent<dynamic> stockEvent, ILambdaContext context)
     {
-        context.Logger.LogLine($"StockOnHandEventHandler: {stockEvent.Detail}");
-        return new string("StockOnHandEventHandler Completed");
+        Detail payload = JsonSerializer.Deserialize<Detail>(stockEvent.Detail.ToString());
+
+        context.Logger.LogInformation($"SKU: {payload.dynamodb.NewImage["sku"].S}");
+
+        if (payload.dynamodb.NewImage["soh"].N != null && int.Parse(payload.dynamodb.NewImage["soh"].N) > 0)
+        {
+            context.Logger.LogInformation("Stock is available");
+            return "Stock is available";
+        }
+        else
+        {
+            context.Logger.LogError("Stock is not available");
+            return "Stock is not available";
+        }
     }
 }
